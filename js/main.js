b9237ad34f7db8e9769f85da47e9a1b47dbc322f
balabaev00 <balabaev00@yandex.ru>
2023-11-17 07:29:38 +0900

    fix

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
 console.log('Im here')
+/**Инициализируем свайпер*/
+new Swiper('.documents__slide-content', {
+    // Автоматическая высота
+    // autoHeight: true,
+
+    // Кол-во слайдов на экране
+    slidesPerView: 3,
+
+    // Растояние между слайдами
+    spaceBetween: 15,
+
+    // По центру распологать слайды
+    centeredSlides: 'true',
+
+    fade: 'true',
+    grapCoursor: 'true',
+
+    // Стартовый слайд
+    initialSlide: 0,
+
+    // Бесконечный прокрут
+    loop: true,
+
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: 'true',
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        }
+    }
+});
